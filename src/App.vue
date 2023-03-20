851f941c949dfc283524c07f66212e38d002e959
Rene-Ali <119358215+Rene-Ali@users.noreply.github.com>
2023-03-20 17:43:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  name: 'App',
+  components: {
+    draggable
+  },
+  data(){
+    return{
+      newTask:"",
+      arrBacklog:[
+        {name: "Code Sign Up Page"},
+        {name: "Test Dashboard"},
+        {name: "Style Registration"},
+        {name: "Help with Design"},
+      ],
+      arrInProgress:[],
+      arrTested:[],
+      arrDone:[]
+    }
+  
+  },
+  methods:{
+    add(){
+      if(this.newTask){
+        this.arrBacklog.push({name: this.newTask});
+        this.newTask="";
+      }
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col form-inline">
+        <b-form-input v-model="newTask" placeholder="Enter Task" @keyup.enter="add"></b-form-input>
+      </div>
+    </div>
+<div class="row">
+  <div class="col md-3">
+    <div class="p-2 alert alert-secondary">
+      <h3>Backlog</h3>
+      <draggable class="list-group" :list="arrBacklog" group="tasks">
+        <div class="list-group-item" v-for="element in arrBacklog" :key="element.name">
+        {{ element.name }}
+        </div>
+      </draggable>
+    </div>
+  </div>
+</div>
+
+  </div>
+</template>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/components/Body/Card.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+    <b-container>
+         <b-row class="mt-4">
+            <b-col cols="4">
+               <b-card
+                  title="Card 1"
+                  img-src="https://via.placeholder.com/318x159"
+                   img-alt="Image"
+                  img-top
+                  tag="article"
+                  class="mb-2"
+                  >
+                  <b-card-text>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
+                  </b-card-text>
+               </b-card>
+            </b-col>
+            <b-col cols="4">
+               <b-card
+                  title="Card 2"
+                  img-src="https://via.placeholder.com/318x159"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  class="mb-2"
+                  >
+                  <b-card-text>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
+                  </b-card-text>
+               </b-card>
+            </b-col>
+            <b-col cols="4">
+               <b-card
+                  title="Card 3"
+                  img-src="https://via.placeholder.com/318x159"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  class="mb-2"
+                  >
+                  <b-card-text>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
+                  </b-card-text>
+               </b-card>
+            </b-col>
+            <b-col cols="12">
+               <b-card
+                  class="mt-4"
+                  >
+                  <h4>Customer details</h4>
+                  <b-table></b-table>
+               </b-card>
+            </b-col>
+         </b-row>
+      </b-container>
+    </div>
+</template>
